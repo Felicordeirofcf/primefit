975dd91bb88a47a6827510903fdc2fae36ae26a3
Felipe Cordeiro Ferreira <felicordeirofcf@gmail.com>
2025-06-03 09:04:56 -0300

    Corrige bug na tela de login

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://<SUA-URL>.supabase.co';
-const supabaseKey = '<SUA-CHAVE-ANON>';
+const supabaseUrl = 'https://vmvqdzxvbmhugolcyojw.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtdnFkenh2Ym1odWdvbGN5b2p3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5MDkxMzMsImV4cCI6MjA2NDQ4NTEzM30.CdMzMmHSGmshPAse2NzK7mvqHcegpe3-HfTc6wdNWq8';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
